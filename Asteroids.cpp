e2042187ddf7041eb3e2ba3023125ed36a6c79b3
lyuboraykov <lyubo.raykov@gmail.com>
2014-01-10 00:08:33 +0200

    Reformatting and scrollng bug fix

--- Asteroids.cpp
+++ Asteroids.cpp
@@ -14,7 +14,7 @@ typedef vector<GameObject>::const_iterator const_iterator;
 
 // Window constants
 const int WINDOW_WIDTH = 70;
-const int WINDOW_HEIGHT = 30;
+const int WINDOW_HEIGHT = 27;
 // Asteroid variables
 const char AsteroidSymbol = '#';
 int asteroidSpeed = 1;
@@ -33,99 +33,102 @@ unsigned int asteroidSpawnInterval = 10;
 
 void Update()
 {
-        COORD direction = { 0, 0 };
-        if (kbhit())
-        {
-                char key = getch();
-                switch (key)
-                {
-                case 'a':
-                        direction.X = -shipSpeed;
-                        direction.Y = 0;
-                        break;
-                case 'w':
-                        direction.X = 0;
-                        direction.Y = -shipSpeed;
-                        break;
-                case 'd':
-                        direction.X = shipSpeed;
-                        direction.Y = 0;
-                        break;
-                case 's':
-                        direction.X = 0;
-                        direction.Y = shipSpeed;
-                        break;
-                };
-        }
-        for (randomAccess_iterator shipBody = ship.begin(); shipBody != ship.end(); ++shipBody)
-        {
-                shipBody->Coordinates.X += direction.X;
-                shipBody->Coordinates.Y += direction.Y;
-        }
+	COORD direction = { 0, 0 };
+	if (kbhit())
+	{
+		char key = getch();
+		switch (key)
+		{
+		case 'a':
+			direction.X = -shipSpeed;
+			direction.Y = 0;
+			break;
+		case 'w':
+			direction.X = 0;
+			direction.Y = -shipSpeed;
+			break;
+		case 'd':
+			direction.X = shipSpeed;
+			direction.Y = 0;
+			break;
+		case 's':
+			direction.X = 0;
+			direction.Y = shipSpeed;
+			break;
+		};
+	}
+	for (randomAccess_iterator shipBody = ship.begin(); shipBody != ship.end(); ++shipBody)
+	{
+		shipBody->Coordinates.X += direction.X;
+		shipBody->Coordinates.Y += direction.Y;
+	}
 
-        // Update the position of all asteroids. Remove any asteroid that goes outside the window
+	// Update the position of all asteroids. Remove any asteroid that goes outside the window
 
-        for (randomAccess_iterator asteroid = asteroids.begin(); asteroid != asteroids.end(); /* Empty clause so we can delete elements */)
-        {
-                asteroid->Coordinates.X -= asteroidSpeed;
-                if (asteroid->Coordinates.X <= 0)
-                {
-                        asteroid = asteroids.erase(asteroid);
-                }
-                else
-                {
-                        ++asteroid;
-                }
-        }
 
-        if (frameCounter % asteroidSpawnInterval == 0)
-        {
-                // Spawn a new asteroid at every x frames
-                int y = rand() % WINDOW_HEIGHT;
-                asteroids.push_back(GameObject(WINDOW_WIDTH - 1, y, AsteroidSymbol));
-        }
-        ++frameCounter;
+	for (randomAccess_iterator asteroid = asteroids.begin(); asteroid != asteroids.end();)
+	{
+		asteroid->Coordinates.X -= asteroidSpeed;
+		if (asteroid->Coordinates.X <= 0)
+		{
+			asteroid = asteroids.erase(asteroid);
+		}
+		else
+		{
+			++asteroid;
+		}
+	}
+
+	if (frameCounter % asteroidSpawnInterval == 0)
+	{
+		// Spawn a new asteroid at every x frames
+		int y = rand() % WINDOW_HEIGHT;
+		asteroids.push_back(GameObject(WINDOW_WIDTH - 1, y, AsteroidSymbol));
+	}
+	++frameCounter;
 }
 
 void Draw()
 {
-        ClearScreen(consoleHandle);
+	ClearScreen(consoleHandle);
 
-        for (const_iterator shipBody = ship.cbegin(); shipBody != ship.cend(); ++shipBody)
-        {
-                shipBody->Draw(consoleHandle);
-        }
+	for (const_iterator shipBody = ship.cbegin(); shipBody != ship.cend(); ++shipBody)
+	{
+		shipBody->Draw(consoleHandle);
+	}
 
-        for (const_iterator asteroid = asteroids.cbegin(); asteroid != asteroids.cend(); ++asteroid)
-        {
-                asteroid->Draw(consoleHandle);
-        }
+	for (const_iterator asteroid = asteroids.cbegin(); asteroid != asteroids.cend(); ++asteroid)
+	{
+		asteroid->Draw(consoleHandle);
+	}
 
 }
 
 int main()
 {
-        consoleHandle = GetStdHandle( STD_OUTPUT_HANDLE );
+	consoleHandle = GetStdHandle( STD_OUTPUT_HANDLE );
 
-        srand(time(NULL));
+	srand(time(NULL));
 
-        int shipY = WINDOW_HEIGHT / 2;
-        int shipX = 1;
-        char shipSymbol = '*';
+	int shipY = WINDOW_HEIGHT / 2;
+	int shipX = 2;
+	char shipSymbol = '*';
 
-        ship.push_back(GameObject(shipX - 1, shipY - 1, shipSymbol));
-        ship.push_back(GameObject(shipX + 1, shipY - 1, shipSymbol));
-        ship.push_back(GameObject(shipX, shipY, shipSymbol));
-        ship.push_back(GameObject(shipX - 1, shipY + 1, shipSymbol));
-        ship.push_back(GameObject(shipX + 1, shipY + 1, shipSymbol));
-        
+	ship.push_back(GameObject(shipX - 2, shipY - 1, shipSymbol));
+	ship.push_back(GameObject(shipX - 1, shipY - 1, shipSymbol));
+	ship.push_back(GameObject(shipX + 1, shipY - 1, shipSymbol));
+	ship.push_back(GameObject(shipX, shipY, shipSymbol));
+	ship.push_back(GameObject(shipX - 1, shipY + 1, shipSymbol));
+	ship.push_back(GameObject(shipX - 2, shipY + 1, shipSymbol));
+	ship.push_back(GameObject(shipX + 1, shipY + 1, shipSymbol));
 
-        while (true)
-        {
-                Update();
-                Draw();
-                Sleep(sleepDuration);
-        }
 
-        return 0;
+	while (true)
+	{
+		Update();
+		Draw();
+		Sleep(sleepDuration);
+	}
+
+	return 0;
 }
