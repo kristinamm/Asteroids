ea78a0108a6e74a8ced38e505a75c984070644f7
Stefan Sinapov <stefan.sinapov@gmail.com>
2014-01-20 10:33:58 +0200

    Added some menu changes

--- Menu.cpp
+++ Menu.cpp
@@ -12,7 +12,7 @@
 
 const std::string Logo[9] = {
 	//Sorry, but i have to put two backslashes when i want to display only one. It doesn't look as good as i would want in the code
-	"                                                              L.Ray\n",
+	"                                                              L.Raykov, S.Sinapov, K.Mileva, K.Kartuleva\n",
 	"                             _                 _     _                           \n",
 	"                   /\\       | |               (_)   | |                         \n",
 	"                  /  \\   ___| |_ ___ _ __ ___  _  __| |___                      \n",
@@ -21,6 +21,15 @@ const std::string Logo[9] = {
 	"               /_/    \\_\\\___/\\__\\___|_|  \\___/|_|\\__,_|___/               \n",
 	"  \n",
 	"  \n",
+};
+
+const std::string MainMenu[5] =
+{
+	"                             1 - Start game                                     \n",
+	"                             2 - Instructions                                   \n",
+	"                             3 - Hightest score                                 \n",
+	"                             4 - Options                                        \n",
+	"                             5 - Quit                                           \n",
 };
 bool ShowIntro = true;
 
@@ -92,13 +101,14 @@ void Menu::Initialize()
 	Beep(300, 50); Beep(400, 400);
 
 
-	//std::cout << std::setw(39) << "MAIN MENU"<< std::endl;
-	//std::cout << std::setw(57) << "Please make your selection"<< std::endl;
-	std::cout << "1 - Start game\n";
-	std::cout << "2 - Instructions\n";
-	std::cout << "3 - Hightest score\n";
-	std::cout << "4 - Options\n";
-	std::cout << "5 - Quit\n";
+	if (MainMenu != NULL)
+	{
+		std::cout << MainMenu[0];
+		std::cout << MainMenu[1];
+		std::cout << MainMenu[2];
+		std::cout << MainMenu[3];
+		std::cout << MainMenu[4];
+	}
 
 	do
 	{
@@ -118,12 +128,11 @@ void Menu::Initialize()
 			std::cout << "Highest score";
 			break;
 		case '4':
-			std::cout << "Settings";
+			Menu::Options();
 			break;
 		case '5':
-			//exit
+			exit(EXIT_SUCCESS);
 			return;
-			break;
 		}
 	} while (key != '1' || key != '5');
 }
@@ -150,7 +159,11 @@ void Menu::Instructions()
 	std::cout << "Hello, player! The destiny of Earth is in your hands!"<<std::endl;
 	std::cout << "Your mission is to destroy as many asteroids as you can.\n";
 	std::cout << "During the game there are more asteroids, they get faster, and they break up into more pieces.\n";
-	std::cout << "Use the W A S D keys on your keyboard to move."<<std::endl;
+	std::cout << "Use the";
+	ColorRed();
+	std::cout << " W A S D ";
+	ColorCyan();
+	std::cout<<"keys on your keyboard to move." << std::endl;
 	std::cout << "With A you move left, with D - right, with S - up and with W - down. Or change the keys from settings.\n";
 	std::cout << "Use the space key  to fire."<<std::endl;
 	std::cout << "Hit P for pause." << std::endl;
@@ -182,7 +195,7 @@ void Menu::Options()
 	std::cout << std::setw (57) << "Please make your selection"<< std::endl;
 	std::cout << " 1 - Play sounds\n";
 	std::cout << " 2 - Stop sounds\n";
-	std::cout << " 3 - Exit\n";
+	std::cout << " 3 - Back\n";
 	char key;
 	do
 	{
@@ -197,9 +210,8 @@ void Menu::Options()
 	//		SoundsOff;
 			return;
 		case '3':
-	//		exitGame = true;
+			Menu::Initialize();
 			return;
-			break;
 		}
 	}
 	while ( key != '1' || key != '3');
